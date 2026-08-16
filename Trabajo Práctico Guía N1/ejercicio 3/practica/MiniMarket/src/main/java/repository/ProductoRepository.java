f9e77c9f0883a6415641c102ecc943fb1f23d4b2
franciscoAragona <162379333+franciscoAragona@users.noreply.github.com>
2026-08-16 19:07:19 -0300

    ejercicio f

---
+++ Trabajo Práctico Guía N1/ejercicio 3/practica/MiniMarket/src/main/java/repository/ProductoRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository;
+
+import com.mycompany.minimarket.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    // Podés agregar búsquedas personalizadas si lo necesitás en el futuro
+    // Ej: List<Producto> findByNombreContainingIgnoreCase(String nombre);
+}
