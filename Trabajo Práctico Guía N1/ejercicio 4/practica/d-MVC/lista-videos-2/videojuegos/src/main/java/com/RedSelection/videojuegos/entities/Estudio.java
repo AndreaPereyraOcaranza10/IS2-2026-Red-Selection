d4c5e33af15c33de9d9d1facf79e9d3a96a04a38
AndreaPereyraOcaranza10 <andrea.a.pereyra02@gmail.com>
2026-08-23 03:00:46 -0300

    Ejercicio lista de reproduccion 2 completo

---
+++ Trabajo Práctico Guía N1/ejercicio 4/practica/d-MVC/lista-videos-2/videojuegos/src/main/java/com/RedSelection/videojuegos/entities/Estudio.java
@@ -0,0 +1,27 @@
+package com.RedSelection.videojuegos.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "estudios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Estudio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+    private boolean activo = true;
+
+    @OneToMany(mappedBy = "estudio")
+    private List<Videojuego> videojuegos;
+}
