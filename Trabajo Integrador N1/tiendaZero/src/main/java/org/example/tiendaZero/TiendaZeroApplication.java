8b1b97467b7265aa911a8ce2892626d84a9823e5
AndreaPereyraOcaranza10 <andrea.a.pereyra02@gmail.com>
2026-09-06 20:59:32 -0300

    commit inicial integrador

---
+++ Trabajo Integrador N1/tiendaZero/src/main/java/org/example/tiendaZero/TiendaZeroApplication.java
@@ -0,0 +1,13 @@
+package org.example.tiendaZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaZeroApplication.class, args);
+	}
+
+}
---
+++ Trabajo Integrador N1/tiendaZero/src/test/java/org/example/tiendaZero/TiendaZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tiendaZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
